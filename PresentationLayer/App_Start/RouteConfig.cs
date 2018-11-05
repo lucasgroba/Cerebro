227b5fedcc23774d0af76a3e08e36820b5e2b0b2
Unknown <lucasgroba9@hotmail.com>
2018-11-05 08:30:46 -0300

    con pruebas en capa de presentacion

--- PresentationLayer/App_Start/RouteConfig.cs
+++ PresentationLayer/App_Start/RouteConfig.cs
@@ -18,6 +18,10 @@ namespace PresentationLayer
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+           name: "Empresa",
+           url: "{controller}/{action}/{name}/{id}"
+       );
         }
     }
 }
---
+++ PresentationLayer/Controllers/EmpresaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
