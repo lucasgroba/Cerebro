32dcc808b08786330f83b719f83e091099cb7930
joaquin <joaco-ferraro@hotmail.com>
2018-11-10 19:02:05 -0300

    altas agregados
    aparecen en la barra una vez logueados

---
+++ WebPresentation/EmpleadosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer;
+
+namespace WebPresentation
+{
+    public class EmpleadosController : Controller
+    {
+        private CEREBROEntities1 db = new CEREBROEntities1();
+
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            var empleados = db.Empleados.Include(e => e.Empresas);
+            return View(empleados.ToList());
+        }
+
+        // GET: Empleados/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // GET: Empleados/Create
+        public ActionResult Create()
+        {
+            ViewBag.RUT_Empresa = new SelectList(db.Empresas, "RUT", "Nombre");
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Ci,Nombre,Fecha_Nac,Tel,Direccion,Activo,RUT_Empresa")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empleados.Add(empleados);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RUT_Empresa = new SelectList(db.Empresas, "RUT", "Nombre", empleados.RUT_Empresa);
+            return View(empleados);
+        }
+
+        // GET: Empleados/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RUT_Empresa = new SelectList(db.Empresas, "RUT", "Nombre", empleados.RUT_Empresa);
+            return View(empleados);
+        }
+
+        // POST: Empleados/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Ci,Nombre,Fecha_Nac,Tel,Direccion,Activo,RUT_Empresa")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleados).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RUT_Empresa = new SelectList(db.Empresas, "RUT", "Nombre", empleados.RUT_Empresa);
+            return View(empleados);
+        }
+
+        // GET: Empleados/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleados empleados = db.Empleados.Find(id);
+            db.Empleados.Remove(empleados);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebPresentation/EmpresasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer;
+
+namespace WebPresentation
+{
+    public class EmpresasController : Controller
+    {
+        private CEREBROEntities1 db = new CEREBROEntities1();
+
+        // GET: Empresas
+        public ActionResult Index()
+        {
+            return View(db.Empresas.ToList());
+        }
+
+        // GET: Empresas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresas empresas = db.Empresas.Find(id);
+            if (empresas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresas);
+        }
+
+        // GET: Empresas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Empresas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RUT,Nombre,Zona_Latitud,Zona_Longitud,Activo")] Empresas empresas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empresas.Add(empresas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(empresas);
+        }
+
+        // GET: Empresas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresas empresas = db.Empresas.Find(id);
+            if (empresas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresas);
+        }
+
+        // POST: Empresas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RUT,Nombre,Zona_Latitud,Zona_Longitud,Activo")] Empresas empresas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empresas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(empresas);
+        }
+
+        // GET: Empresas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresas empresas = db.Empresas.Find(id);
+            if (empresas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresas);
+        }
+
+        // POST: Empresas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empresas empresas = db.Empresas.Find(id);
+            db.Empresas.Remove(empresas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
